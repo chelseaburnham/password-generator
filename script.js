45bf03c1101e36bf81d4c6f846e3e6db824a1efb
Chelsea Burnham <chelseaburnham0@gmail.com>
2022-06-22 17:18:35 -0700

    create characters object with arrays

--- script.js
+++ script.js
@@ -1,5 +1,11 @@
 // Assignment code here
 
+var characters = {
+  upper: ["A", "B", "C", "D", "E", "F", "G", "H", "I", "J", "K", "L", "M", "N", "O", "P", "Q", "R", "S", "T", "U", "V", "W", "X", "Y", "Z"], 
+  lower: ["a", "b", "c", "d", "e", "f", "g", "h", "i", "j", "k", "l", "m", "n", "o", "p", "q", "r", "s", "t", "u", "v", "w", "x", "y", "z"], 
+  special: ["!", "@", "#", "$", "%", "?", "*", "&"], 
+  number: [0, 1, 2, 3, 4, 5, 6, 7, 8, 9],
+};
 
 // Get references to the #generate element
 var generateBtn = document.querySelector("#generate");
@@ -10,8 +16,8 @@ function writePassword() {
   var passwordText = document.querySelector("#password");
 
   passwordText.value = password;
-
 }
 
 // Add event listener to generate button
 generateBtn.addEventListener("click", writePassword);
+
