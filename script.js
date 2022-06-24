8132de006cb8dc5fc501393ca0dcb6db2508e028
Chelsea Burnham <chelseaburnham0@gmail.com>
2022-06-23 23:02:36 -0700

    add if statements for characters variable

--- script.js
+++ script.js
@@ -1,30 +1,113 @@
 // Assignment code here
 
-var characters = {
-  upper: ["A", "B", "C", "D", "E", "F", "G", "H", "I", "J", "K", "L", "M", "N", "O", "P", "Q", "R", "S", "T", "U", "V", "W", "X", "Y", "Z"], 
-  lower: ["a", "b", "c", "d", "e", "f", "g", "h", "i", "j", "k", "l", "m", "n", "o", "p", "q", "r", "s", "t", "u", "v", "w", "x", "y", "z"], 
-  special: ["!", "@", "#", "$", "%", "?", "*", "&"], 
-  number: [0, 1, 2, 3, 4, 5, 6, 7, 8, 9],
-};
+  var special = ["!", "@", "#", "$", "%", "?", "*", "&"];
+  var upper = ["A", "B", "C", "D", "E", "F", "G", "H", "I", "J", "K", "L", "M", "N", "O", "P", "Q", "R", "S", "T", "U", "V", "W", "X", "Y", "Z"];
+  var lower = ["a", "b", "c", "d", "e", "f", "g", "h", "i", "j", "k", "l", "m", "n", "o", "p", "q", "r", "s", "t", "u", "v", "w", "x", "y", "z"];
+  var number = ["0", "1", "2", "3", "4", "5", "6", "7", "8", "9"];
 
 function generatePassword() {
   var characterLength = prompt("How many characters would you like in your password?");
-  if (characterLength < 8) {
-    alert("You must have more than 8 characters.")
-  };
-  if (characterLength > 128) {
-    alert("You must have less than 128 characters.")
-  };
-  if (characterLength > 8 && characterLength < 128) {
-    var characterSpecial = confirm("Would you like special characters?")
+  while (characterLength < 8 || characterLength > 128) {
+    alert("You must have between 8 and 128 characters. Try again.")
+    characterLength = prompt("How many characters would you like in your password?");
+  } if (characterLength >= 8 && characterLength <= 128) {
+    alert("Ok great! You chose " + characterLength + " characters.")
   };
 
-  /* 1. window prompt for criteria (length between 8 and 128 characters)
-    2. confirm characters (lowercase, uppercase, special, and numeric)
-    3. generate the password */
+  var characterSpecial = confirm("Would you like to include special characters?");
+  var characterUpper = confirm("Would you like to include uppercase letters?");
+  var characterLower = confirm("Would you like to include lowercase letters?");
+  var characterNumber = confirm("Would you like to include numbers?");
 
-  return ;
+while (characterSpecial === false && characterUpper === false && characterLower === false && characterNumber === false) {
+    alert("You must choose at least one of the given options.");
+    characterSpecial = confirm("Would you like to include special characters?");
+    characterUpper = confirm("Would you like to include uppercase letters?");
+    characterLower = confirm("Would you like to include lowercase letters?");
+    characterNumber = confirm("Would you like to include numbers?");
+};
+
+var characters;
+
+// probably not the most efficient, but this is gathering the user responses into the characters variable
+if (characterSpecial && characterUpper && characterLower && characterNumber) {
+  characters = special.concat(upper, number, lower);
+};
+
+if (characterSpecial && !characterUpper && characterLower && characterNumber) {
+  characters = special.concat(lower, number);
+};
+
+if (!characterSpecial && characterUpper && characterLower && characterNumber) {
+  characters = upper.concat(lower, number);
+};
+
+if (characterSpecial && characterUpper && !characterLower && characterNumber) {
+  characters = special.concat(upper, number);
+};
+
+if (characterSpecial && characterUpper && characterLower && !characterNumber) {
+  characters = special.concat(upper, lower);
+};
+
+if (characterSpecial && !characterUpper && !characterLower && !characterNumber) {
+  characters = special;
+};
+
+if (!characterSpecial && characterUpper && !characterLower && !characterNumber) {
+  characters = upper;
+};
+
+if (!characterSpecial && !characterUpper && characterLower && !characterNumber) {
+  characters = lower;
+};
+
+if (!characterSpecial && !characterUpper && !characterLower && characterNumber) {
+  characters = number;
+};
+
+if (characterSpecial && characterUpper && !characterLower && !characterNumber) {
+  characters = special.concat(upper);
+};
+
+if (characterSpecial && !characterUpper && characterLower && !characterNumber) {
+  characters = special.concat(lower);
+};
+
+if (characterSpecial && !characterUpper && !characterLower && characterNumber) {
+  characters = special.concat(number);
+};
+
+if (!characterSpecial && characterUpper && characterLower && !characterNumber) {
+  characters = upper.concat(lower);
+};
+
+if (!characterSpecial && characterUpper && !characterLower && characterNumber) {
+  characters = upper.concat(number);
+};
+
+if (!characterSpecial && !characterUpper && characterLower && characterNumber) {
+  characters = lower.concat(number);
+};
+
+for (var i = 0; i < characterLength.length; i++) {
+  randomPassword = characters[Math.floor(Math.random() * characterLength)];
 }
+
+return randomPassword;
+}
+
+
+
+
+
+
+
+
+
+
+
+
 
 // Get references to the #generate element
 var generateBtn = document.querySelector("#generate");
